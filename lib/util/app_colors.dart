bea9046030ebede93d54031ccbe66b4d093829b1
mokh1999 <elmaghwry37@gmail.com>
2025-03-03 13:44:25 +0200

    first commit

---
+++ lib/util/app_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+/// ⭐Color⭐
+abstract class AppColors {
+  //#344054D9
+  static const Color primaryColor = Color(0xFFCA3652);
+  static const Color transparent = Colors.transparent;
+
+
+  static const Color orange = Color(0xFFE28830);
+  static const Color subPrimary =  Color(0xFFD22C50);
+  static const Color purple =  Color(0xFFB8267B);
+  static const Color subPurple = Color(0xFF951D7E);
+  static const Color blue = Color(0xFF4286C5);
+
+//
+// static const Color gray = Color(0xFF3D3D3D);
+// static const Color grayAppBar = Color(0xFFB2B2B2);
+// static const Color grayContainer = Color(0xFFF7F7F7);
+// static const Color grayHintText = Color(0xffADADAD);
+// static const Color grayMainText = Color(0xff404040);
+// static const Color orange = Color(0xFFFF7B38);
+// static const Color white = Color(0xFFFFFFff);
+// static const Color whiteBackGround = Color(0xFFEFEFEF);
+// static const Color black = Color(0xFF000000);
+// static const Color containerBackground = Color(0xFF3D3D3D);
+// static const Color blue = Color(0xFF36A0EF);
+// static const Color blueLight = Color(0xFFBBEEFF);
+// static const Color blueLight2 = Color(0xFFEDF8F8);
+// static const Color cardColor = Color(0xFF4AB7B7);
+// static const Color gray2 = Color(0xFF515151);
+// static const Color red = Color(0xFFD90026);
+// static const Color grayMessage = Color(0xFFF1F1F1);
+// static const Color blueMessage = Color(0xFFEDF8F8);
+// static const Color containerSelected = Color(0xFFEEECF4);
+}
